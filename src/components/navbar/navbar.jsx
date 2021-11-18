b8090119a65859e147a200d558914fdd484c88b8
Liam R <liamroberts77@googlemail.com>
2021-11-18 19:38:09 +0000

    created folder and file navbar

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button, Menu, Typography, Avatar } from "antd";
+
+const navbar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default navbar
