2ddb45855869981796915e276c956846a3b999a1
Liam R <liamroberts77@googlemail.com>
2021-11-18 20:37:38 +0000

    added menu and items to navbar.jsx

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter as Router } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />,document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,document.getElementById('root')
+);
 
 reportWebVitals();
