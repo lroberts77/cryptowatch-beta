1c2352726ea4f9416742f9cbef21d92d3d9dc1ff
Liam R <liamroberts77@googlemail.com>
2021-11-26 21:45:54 +0000

    added files for services and store

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import millify from 'millify'
-import { Typography, Row, Colummn, Statistic } from 'antd'
+import { Typography, Row, Col, Statistic } from 'antd'
+import { Link } from "react-router-dom";
+
+const { Title } = Typography
 
 const Homepage = () => {
     return (
-        <div>
-            home
-        </div>
+        <>
+            <Title level={2} className='heading'>Global Crytpo Stats</Title>
+            <Row>
+                <Col span={12}><Statistic title='Total Cryptocurrencies' value='5'></Statistic></Col>
+                <Col span={12}><Statistic title='Total Exchanges' value='5'></Statistic></Col>
+                <Col span={12}><Statistic title='Total Market Cap' value='5'></Statistic></Col>
+                <Col span={12}><Statistic title='Total 24h Volume' value='5'></Statistic></Col>
+                <Col span={12}><Statistic title='Total Markets' value='5'></Statistic></Col>
+                <Col span={12}><Statistic title='Total Cryptocurrencies' value='5'></Statistic></Col>
+            </Row>
+        </>
     )
 }
 
---
+++ src/services/cryptoApi.jsx
@@ -0,0 +1,10 @@
+
+
+// var options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/exchanges',
+//     headers: {
+//       'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//       'x-rapidapi-key': '8fc63b4210mshb84242c171adc21p17e6e8jsn0eaeee4ed843'
+//     }
+//   };
