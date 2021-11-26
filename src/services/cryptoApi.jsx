23863059a4a13a6d6f96c4c061fdd4078373277b
Liam R <liamroberts77@googlemail.com>
2021-11-26 22:21:48 +0000

    added api fetch in cryptoApi.jsx

--- src/services/cryptoApi.jsx
+++ src/services/cryptoApi.jsx
@@ -1,10 +1,20 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
 
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '8fc63b4210mshb84242c171adc21p17e6e8jsn0eaeee4ed843'
+}
 
-// var options = {
-//     method: 'GET',
-//     url: 'https://coinranking1.p.rapidapi.com/exchanges',
-//     headers: {
-//       'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-//       'x-rapidapi-key': '8fc63b4210mshb84242c171adc21p17e6e8jsn0eaeee4ed843'
-//     }
-//   };
+const baseUrl = 'https://coinranking1.p.rapidapi.com/exchanges'
+
+const createRquest = (url) => ({ url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRquest('/exchanges')
+        })
+    })
+})
