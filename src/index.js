a0e55ed207ef34f4b7c6895a0fc32b28c9ba01b0
Liam R <liamroberts77@googlemail.com>
2021-11-26 21:49:55 +0000

    imported store and Provider in index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
-import 'antd/dist/antd.css'
+import { Provider } from "react-redux";
+import store from './app/store';
 import reportWebVitals from './reportWebVitals';
 import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+      <Provider store={store}>
         <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root'),
