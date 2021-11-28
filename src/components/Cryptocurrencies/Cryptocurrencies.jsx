dea385becaed364f464b5b769f9a828050489ac3
Liam R <liamroberts77@googlemail.com>
2021-11-28 00:12:53 +0000

    added imports , api fetch and state to cryptocurrencies.js

--- src/components/Cryptocurrencies/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies/Cryptocurrencies.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import millify from 'millify';
 import { Link } from 'react-router-dom';
 import { Card, Row, Col, Input } from 'antd';
 import { useGetCryptosQuery } from '../../services/cryptoApi';
 
-
-
 const Cryptocurrencies = () => {
+
+    const { data: cryptoList, isFetching } = useGetCryptosQuery();
+    const [cryptos, setcryptos] = useState(cryptoList?.data?.coins);
+
     return (
         <div>
             Cryptocurrencies
@@ -14,4 +16,4 @@ const Cryptocurrencies = () => {
     )
 }
 
-export default Cryptocurrencies
+export default Cryptocurrencies;
