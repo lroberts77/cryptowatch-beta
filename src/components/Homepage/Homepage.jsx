1da29f05095f2a7a33d284dcd540ff516e9b7adf
Liam R <liamroberts77@googlemail.com>
2021-11-30 23:53:00 +0000

    uodated homepage

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -17,7 +17,7 @@ const Homepage = () => {
 
     return (
         <>
-            <Title level={2} className='heading'>Global Crytpo Stats</Title>
+            <Title level={2} className='heading'>Global Crypto Stats</Title>
             <Row>
                 <Col span={12}><Statistic title='Total Cryptocurrencies' value={globalStats.total}></Statistic></Col>
                 <Col span={12}><Statistic title='Total Exchanges' value={globalStats.totalExchanges}></Statistic></Col>
