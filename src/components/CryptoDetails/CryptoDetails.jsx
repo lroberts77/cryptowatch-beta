9d4b30f39c6289014f5888ee760cd4551b482160
Liam R <liamroberts77@googlemail.com>
2021-12-14 21:41:59 +0000

    added props to pass to linechart in cryptodetails.jsx

--- src/components/CryptoDetails/CryptoDetails.jsx
+++ src/components/CryptoDetails/CryptoDetails.jsx
@@ -5,7 +5,7 @@ import millify from 'millify';
 import { Col, Row, Typography, Select } from 'antd';
 import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
 import { useGetCryptoDetailsQuery } from '../../services/cryptoApi';
-import { Link } from 'react-router-dom';
+import LineChart from '../LineChart/LineChart';
 
 const { Title, Text } = Typography;
 const { Option } = Select;
@@ -60,7 +60,7 @@ const CryptoDetails = () => {
             >
                 {time.map((date) => <Option key={date}>{date}</Option>)}
             </Select>
-            {/* line chart */}
+            <LineChart currentPrice={millify(cryptoDetails.price)} coinName={cryptoDetails.name} />
             <Col className='stats-container'>
                 <Col className='coin-value-statistics'>
                     <Col className='coin-value-statistics-heading'>
--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as Exchanges } from "./Exchanges/Exchanges";
 export { default as Homepage } from "./Homepage/Homepage";
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as News } from "./News/News";
+export { default as LineChart } from "./LineChart/LineChart";
