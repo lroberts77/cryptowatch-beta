ab73e6de69105a81eb509f18757db0807910b849
Liam R <liamroberts77@googlemail.com>
2022-01-06 14:15:14 +0000

    added current price in column in linechart.jsx

--- src/components/LineChart/LineChart.jsx
+++ src/components/LineChart/LineChart.jsx
@@ -10,7 +10,8 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
             <Row className='chart-header'>
                 <Title level={5} className='chart-title'>{coinName} Price Chart</Title>
                 <Col className='price-container'>
-                <Title level={5} className='price-change'>{coinHistory?.data?.change}%</Title>
+                    <Title level={5} className='price-change'>{coinHistory?.data?.change}%</Title>
+                    <Title level={5} className='current-price'>Current {coinName} Price: $ {currentPrice}</Title>
                 </Col>
             </Row>
         </>
